aee271c234ee7af093faf4009ae1c6e4ded72caf
sjojon <90796535+sjojon@users.noreply.github.com>
2023-05-26 14:03:57 +0200

    Gjort ferdig unit tester

--- Ukesoppgaveuke4.Test/UnitTestDealership.cs
+++ Ukesoppgaveuke4.Test/UnitTestDealership.cs
@@ -1,68 +1,155 @@
-﻿namespace Ukesoppgaveuke4.Test
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Ukesoppgaveuke4.Test
 {
     public class UnitTestDealership
     {
+        private Dealership dealership;
+        private Car car1;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            dealership = new Dealership("Auto", "Hollywood", new List<Car>
+            {
+                new("Mercedes", "ABC123", 2010, 100000),
+                new("Toyota", "DEF456", 2014, 85000),
+                new("Mazda", "GHI789", 2019, 43900),
+                new("Ford", "JKL012", 2008, 75000),
+                new("Chevrolet", "MNO345", 2012, 60000)
+            });
+
+            car1 = new Car("Land Rover", "FWP385", 2005, 430000);
+        }
+
         [Test]
         public void TestDisplayAllCars()
         {
-            Dealership dealership = new("Auto", "Hollywood", new List<Car>());
-
-            Car car1 = new("Mercedes", "ABC123", 2010, 100000);
-            Car car2 = new("Toyota", "DEF456", 2014, 85000);
-            Car car3 = new("Mazda", "GHI789", 2019, 43900);
-
-            dealership.AddCar(car1);
-            dealership.AddCar(car2);
-            dealership.AddCar(car3);
+            //Dealership dealership = new("Auto", "Hollywood", new List<Car>());
+            //Car car1 = new("Mercedes", "ABC123", 2010, 100000);
+            //Car car2 = new("Toyota", "DEF456", 2014, 85000);
+            //Car car3 = new("Mazda", "GHI789", 2019, 43900);
+            //dealership.AddCar(car1);
+            //dealership.AddCar(car2);
+            //dealership.AddCar(car3);
 
 
             dealership.DisplayAllCars();
 
             Assert.That(dealership.Name, Is.EqualTo("Auto"));
             Assert.That(dealership.Address, Is.EqualTo("Hollywood"));
-            Assert.That(dealership.Cars, Has.Count.EqualTo(3));
+            Assert.That(dealership.Cars, Has.Count.EqualTo(5));
         }
 
         [Test]
         public void TestAddCar()
         {
-            Dealership dealership = new("Auto", "Hollywood", new List<Car>());
-
-            Car car1 = new("Land Rover", "FWP385", 2005, 430000);
+            //Dealership dealership = new("Auto", "Hollywood", new List<Car>());
+            //Car car1 = new("Land Rover", "FWP385", 2005, 430000);
 
             dealership.AddCar(car1);
-
-            Assert.That(dealership.Cars, Has.Count.EqualTo(1));
+            Assert.That(dealership.Cars, Has.Count.EqualTo(6));
         }
 
         [Test]
         public void TestSellCar()
         {
-            Dealership dealership = new("Auto", "Hollywood", new List<Car>());
-
-            Car car1 = new("Land Rover", "FWP385", 2005, 430000);
+            //Dealership dealership = new("Auto", "Hollywood", new List<Car>());
+            //Car car1 = new("Land Rover", "FWP385", 2005, 430000);
 
             dealership.SellCar(car1);
-
-            Assert.That(dealership.Cars, Has.Count.EqualTo(0));
+            Assert.That(dealership.Cars, Has.Count.EqualTo(5));
         }
 
         [Test]
         public void TestFindCarIndex()
         {
-            Dealership dealership = new("Auto", "Hollywood", new List<Car>());
-            Car car1 = new("Land Rover", "FWP385", 2005, 430000);
-            dealership.AddCar(car1);
+            //Dealership dealership = new("Auto", "Hollywood", new List<Car>());
+            //Car car1 = new("Land Rover", "FWP385", 2005, 430000);
+            //dealership.AddCar(car1);
 
-            string getIndex = "FWP385".ToLower();
+            string getIndex = "GHI789".ToLower();
             int index = dealership.FindCarIndex(getIndex);
-
-            Assert.That(index, Is.EqualTo(0));
+            Assert.That(index, Is.EqualTo(2));
         }
 
         [Test]
         public void TestGetCar()
         {
+            int index = 2;
+            Car result = dealership.GetCar(index);
+
+            Assert.That(result, Is.EqualTo(dealership.Cars[index]));
+        }
+
+        [Test]
+        public void TestGetOlderCars()
+        {
+            const int year = 2011;
+            List<Car> olderCars = dealership.GetOlderCars(year);
+            List<Car> expectedCars = new()
+            {
+                new Car("Mercedes", "ABC123", 2010, 100000),
+                new Car("Ford", "JKL012", 2008, 75000),
+            };
+
+            // Todo: Måtte legge til Equals() og GetHashCode() i Car klassen for å få dette til å virke!
+
+            Assert.That(olderCars.SequenceEqual(expectedCars), Is.True);
+
+            //foreach (var car in olderCars)
+            //{
+            //    car.DisplayCarDetails();
+            //}
+
+            //Console.WriteLine("-----------------------------------------------------------");
+            //foreach (var car in expectedCars)
+            //{
+            //    car.DisplayCarDetails();
+            //}
+
+            //for (int i = 0; i < olderCars.Count; i++)
+            //{
+            //    var olderCar = olderCars[i];
+            //    var expectedCar = expectedCars[i];
+
+            //    if (!olderCar.Equals(expectedCar))
+            //    {
+            //        Console.WriteLine($"Mismatch at index {i}:");
+            //        Console.WriteLine("Actual car:");
+            //        olderCar.DisplayCarDetails();
+            //        Console.WriteLine("Expected car:");
+            //        expectedCar.DisplayCarDetails();
+            //    }
+            //}
+
+            //CollectionAssert.AreEqual(olderCars, expectedCars);
+            //Assert.That(olderCars, Has.Count.EqualTo(expectedCars.Count));
+            //for (var i = 0; i < expectedCars.Count; i++)
+            //{
+            //    Assert.AreEqual(expectedCars[i], olderCars[i]);
+            //    //Assert.That(olderCars[i], Is.EqualTo(expectedCars[i]));
+            //}
+        }
+
+        [Test]
+        public void TestGetNewerCars()
+        {
+            const int km = 90000;
+            List<Car> carsByKm = dealership.GetCarsByKm(km);
+            List<Car> expectedCarsByKm = new()
+            {
+                new Car("Toyota", "DEF456", 2014, 85000),
+                new Car("Mazda", "GHI789", 2019, 43900),
+                new Car("Ford", "JKL012", 2008, 75000),
+                new Car("Chevrolet", "MNO345", 2012, 60000)
+            };
+
+            Assert.That(carsByKm.SequenceEqual(expectedCarsByKm), Is.True);
         }
     }
 }
--- Ukesoppgaveuke4/Car.cs
+++ Ukesoppgaveuke4/Car.cs
@@ -40,4 +40,48 @@ public class Car
     {
         Odometer = newOdometer;
     }
+
+    // Todo: La til disse for å kunne kjøre TestOlderCars testen:
+    // Måtte spørre chatGPT til slutt da jeg ikke hadde sjans til å finne det ut,
+    // og broren min kunne ikke komme på noe han heller i farta.
+    // Men sa nå etterpå at det er ca sånn man må gjøre i Java også.
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Car otherCar = (Car)obj;
+        return Make == otherCar.Make &&
+               RegistrationNumber == otherCar.RegistrationNumber &&
+               Year == otherCar.Year &&
+               Odometer == otherCar.Odometer;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Make, RegistrationNumber, Year, Odometer);
+    }
+
+    /*
+     *
+     * Når du sammenligner to objekter i C# ved hjelp av metoden Equals,
+     * brukes standardimplementeringen av Equals-metoden som er arvet fra System.Object.
+     * Denne standardimplementeringen sammenligner objektreferansene og returnerer true bare hvis
+     * objektene refererer til samme minneadresse. I testen din ønsket du imidlertid å sammenligne
+     * egenskapene til Car-objektene, ikke objektreferansene.
+
+     * Derfor måtte du tilpasse Equals-metoden i Car-klassen slik at den sammenligner egenskapene til bilene. 
+     * Ved å implementere Equals-metoden på denne måten, kan du nå sammenligne to Car-objekter og få riktig
+     * resultat basert på egenskapene de inneholder.
+
+     * Hva med GetHashCode-metoden? I C# brukes GetHashCode-metoden sammen med Equals-metoden for å
+     * sikre at to objekter som er like (ifølge Equals-metoden), har samme hashkode.
+     * Hashkoden brukes ofte i hashtabeller og hashsamlinger for raskt å finne objekter med samme egenskaper.
+     * Ved å implementere GetHashCode-metoden i Car-klassen på en konsistent måte,
+     * sikrer du at to Car-objekter med de samme egenskapene vil ha samme hashkode.
+
+     * Så ved å implementere Equals-metoden og GetHashCode-metoden riktig,
+     * sørget du for at SequenceEqual-metoden kunne sammenligne egenskapene til bilene i stedet for objektreferansene.
+     * Dette gjorde at testen din passerte når de to listene med biler hadde identiske egenskaper.
+     */
 }
